8ddab942759c07fb5dba4e6aced265b2841452f8
Shaik Amaan <95491199+Shaik-Mohammed-Amaan@users.noreply.github.com>
2024-08-05 21:11:58 +0530

    Added Exercise - 6 Spring Project

---
+++ Week - 2/Week 2_Spring Core_Maven/Exercise - 6/LibraryManagement-6/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+    public void bookRepoContents() {
+        System.out.println("Book Repository is currently empty");
+    }
+}
