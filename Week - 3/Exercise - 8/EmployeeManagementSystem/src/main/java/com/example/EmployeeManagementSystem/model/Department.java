8bf79635794377fb68567605e9a421de177ed932
Shaik Amaan <95491199+Shaik-Mohammed-Amaan@users.noreply.github.com>
2024-08-13 20:13:06 +0530

    Added Week - 3 Exercise - 8

---
+++ Week - 3/Exercise - 8/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/model/Department.java
@@ -0,0 +1,49 @@
+package com.example.EmployeeManagementSystem.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "DEPARTMENT")
+@EntityListeners(AuditingEntityListener.class)  // Enabling auditing for this entity
+
+@NamedQueries({
+        @NamedQuery(name = "Department.fetchByName", query = "SELECT D FROM Department D WHERE D.name = :name"),
+        @NamedQuery(name = "Department.fetchDepartmentByNameContaining", query = "SELECT D FROM Department D WHERE D.name LIKE CONCAT('%',:pattern,'%')")
+})
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+}
---
+++ Week - 3/Exercise - 8/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/projections/DepartmentDTO.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeManagementSystem.projections;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentDTO {
+    private int id;
+    private String name;
+}
